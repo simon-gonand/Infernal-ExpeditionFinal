83edfe143a45940c19580322ef29ea0a75a36c78
Simon <simongonand@gmail.com>
2021-11-08 10:02:56 +0100

    add offset when snapping player to treasure

--- Assets/Scripts/Treasures/GetSnappingPosition.cs
+++ Assets/Scripts/Treasures/GetSnappingPosition.cs
@@ -5,9 +5,28 @@ using UnityEngine;
 public class GetSnappingPosition : MonoBehaviour
 {
     public Transform self;
+    public Transform treasureTransform;
 
     public void SnapPlayerToPosition(PlayerController player)
     {
-        player.self.position = new Vector3(self.position.x, player.self.position.y, self.position.z);
+        player.self.position = new Vector3(self.position.x, player.self.position.y, self.position.z); ;
+        Vector3 snapPlayerPosition = treasureTransform.position;
+        if (player.self.position.x < treasureTransform.position.x)
+        {
+            snapPlayerPosition.x += player.self.lossyScale.x / 2;
+        }
+        else if (player.self.position.x > treasureTransform.position.x)
+        {
+            snapPlayerPosition.x -= player.self.lossyScale.x / 2;
+        }
+        else if (player.self.position.z < treasureTransform.position.z)
+        {
+            snapPlayerPosition.z += player.self.lossyScale.z / 2;
+        }
+        else if (player.self.position.z > treasureTransform.position.z)
+        {
+            snapPlayerPosition.z -= player.self.lossyScale.z / 2;
+        }
+        treasureTransform.position = snapPlayerPosition;
     }
 }
