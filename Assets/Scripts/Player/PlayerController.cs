e460a7fda8436ac13729fcd20b42ae39e4cdd2b3
Simon Gonand <simongonand@gmail.com>
2021-12-10 23:22:27 +0100

    carried treasure does not go in meshes

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -99,7 +99,7 @@ public class PlayerController : MonoBehaviour
         if (_isCarrying && collision.collider.GetComponent<IInteractable>() != _interactingWith)
         {
             collisionDirection = collision.GetContact(0).normal;
-            if (!Physics.Raycast(self.position, collisionDirection, 0.1f, mask))
+            if (!Physics.Raycast(self.position, -collisionDirection, 0.1f, mask))
                 return;
             Treasure treasure = _interactingWith as Treasure;
             if (treasure != null)
