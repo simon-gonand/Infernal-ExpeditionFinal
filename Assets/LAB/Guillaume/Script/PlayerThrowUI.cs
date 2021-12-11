9199275bd57e05252f420b21aeea706083b802fb
Guillaume.R <g.roge@rubika-edu.com>
2021-12-11 20:53:47 +0100

    Set up ui

--- Assets/LAB/Guillaume/Script/PlayerThrowUI.cs
+++ Assets/LAB/Guillaume/Script/PlayerThrowUI.cs
@@ -1,8 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerThrowUI : MonoBehaviour
 {
-   
+    public SpriteRenderer activeCircle;
+
+    public Image coneBackGround;
+    public Image coneFiller;
+
+
+
 }
