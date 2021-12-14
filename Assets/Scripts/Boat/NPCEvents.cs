a7ea2afbb0b7c7b8d2154f9893aed49d2ef7b5c8
MathGD <m.vancappellen@rubika-edu.com>
2021-12-14 21:55:28 +0100

    Captain NPC Events are done

--- Assets/Scripts/Boat/NPCEvents.cs
+++ Assets/Scripts/Boat/NPCEvents.cs
@@ -4,13 +4,50 @@ using UnityEngine;
 
 public class NPCEvents : MonoBehaviour
 {
+    [SerializeField] private float time;
+    [SerializeField] private bool displayingToken;
+    public float timeToDisplay;
+
+    [SerializeField] private GameObject displayToken;
+
+    [SerializeField] private GameObject speedUpToken;
+    [SerializeField] private GameObject speedDownToken;
+
+
+    private void Start()
+    {
+        time = 0;
+        displayingToken = false;
+    }
+
+    private void Update()
+    {
+        if (displayingToken)
+        {
+            time += Time.deltaTime;
+            displayToken.SetActive(true);
+        }
+
+        if (time >= timeToDisplay)
+        {
+            displayingToken = false;
+            displayToken.SetActive(false);
+            time = 0;
+        }
+    }
     public void OnSpeedUp()
     {
+        displayToken = speedUpToken;
+
+        displayingToken = true;
         Debug.Log("Speed up !");
     }
 
     public void OnSpeedDown()
     {
+        displayToken = speedDownToken;
+
+        displayingToken = true;
         Debug.Log("Speed down !");
     }
 
