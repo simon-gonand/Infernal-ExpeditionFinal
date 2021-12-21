de86fb0d4618b03e0d19a61783af1c2ddccbefb8
Simon Gonand <simongonand@gmail.com>
2021-12-21 12:36:43 +0100

    clean up code

--- Assets/Scripts/ICarriable.cs
+++ Assets/Scripts/ICarriable.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public interface ICarriable : IInteractable
 {
-    public bool isLoadingLaunch { get; }
-
     public void GetOnBoat(Transform entryPosition);
     public void GetOffBoat();
     public void Launch(PlayerController player);
