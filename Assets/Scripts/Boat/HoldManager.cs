0a599015ac3b8b592033641a5466036dccecee05
Simon <simongonand@gmail.com>
2022-01-10 14:16:05 +0100

    player uninteracts with treasures when bringing directly to boat

--- Assets/Scripts/Boat/HoldManager.cs
+++ Assets/Scripts/Boat/HoldManager.cs
@@ -12,6 +12,14 @@ public class HoldManager : MonoBehaviour
     {
         if (other.CompareTag("Treasures"))
         {
+            Treasure treasure = other.GetComponent<Treasure>();
+            while (treasure.playerInteractingWith.Count > 0)
+            {
+                treasure.playerInteractingWith[0].selfRigidBody.constraints = RigidbodyConstraints.FreezeRotation;
+                treasure.playerInteractingWith[0].selfRigidBody.mass = 1;
+                treasure.playerInteractingWith[0].interactingWith.UninteractWith(treasure.playerInteractingWith[0]);
+            }
+
             // Register in score
             ScoreManager.instance.AddScore(other.gameObject.GetComponent<Treasure>().price);
 
