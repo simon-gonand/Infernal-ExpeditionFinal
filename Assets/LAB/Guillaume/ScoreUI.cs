37f4a60d8df92dc887353f2d5f1bd0ded1cd920f
Guillaume <g.roge@rubika-edu.com>
2022-01-20 00:12:32 +0100

    clean and start new score ui

---
+++ Assets/LAB/Guillaume/ScoreUI.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreUI : MonoBehaviour
+{
+    public Slider selfSlider;
+    public RectTransform handle;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
