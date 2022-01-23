e7920d699fdaa98305c86422be77c37a14b7f451
Simon <simongonand@gmail.com>
2022-01-23 15:07:19 +0100

    fix path in level 04

--- Assets/Scripts/PathScripts/FollowPath.cs
+++ Assets/Scripts/PathScripts/FollowPath.cs
@@ -89,11 +89,7 @@ public class FollowPath : MonoBehaviour
 
             Vector3 rotation = self.position - oldPos;
             rotation.y = 0.0f;
-            if (!CoordiantesComparison(self.position, oldPos))
-            {
-                Debug.Log(rotation);
-                self.rotation = Quaternion.LookRotation(rotation);
-            }
+            self.rotation = Quaternion.LookRotation(rotation);
 
 
             // Camera position
