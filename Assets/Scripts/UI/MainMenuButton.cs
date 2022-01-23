cc09dfa52c59f919e0f29de94c1bfea0cf4f85e4
Simon <simongonand@gmail.com>
2022-01-23 22:20:01 +0100

    change main menu ui skins

---
+++ Assets/Scripts/UI/MainMenuButton.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class MainMenuButton : MonoBehaviour, ISelectHandler, IDeselectHandler
+{
+    [SerializeField]
+    private Image image;
+
+    void ISelectHandler.OnSelect(BaseEventData eventData)
+    {
+        Color color = image.color;
+        color.a = 255.0f;
+        image.color = color;
+    }
+
+    void IDeselectHandler.OnDeselect(BaseEventData eventData)
+    {
+        Color color = image.color;
+        color.a = 0.0f;
+        image.color = color;
+    }
+}
